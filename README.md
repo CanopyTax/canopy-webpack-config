bb1ed8368a1a6558544b1e4c36640a92004c6b7e
Joel Denning <joel.denning@canopytax.com>
2018-08-27 14:08:00 -0600

    Support for analyze mode

--- README.md
+++ README.md
@@ -7,19 +7,19 @@ Some defaults for webpack configs at Canopy.
 - Still be 100% overrideable.
 
 ## Things we don't want
-- Hiding webpack configs to the CRA or angular-cli level. Nothing will need to be "ejected".
+- Hiding webpack configs at a level comparable to create-react-app or angular-cli. Nothing will need to be "ejected," you can always override anything you want.
 - Discouraging people from experimenting with their webpack config.
 - Discouraging people from knowing webpack.
 - Being overly opininated about how you do things like css (or other "autonomy" areas of building a sofe service).
 
 ## Usage
-Create `webpack.config.js` file. Then create a `"build": "webpack --config webpack.config.js"` script in your package.json.
-In your webpack.config.js file, call the `canopyWebpackConfig()` function to get some defaults and add/override to your webpack
-config.
-
+First, install the library as a devDependency.
 ```bash
 yarn add --dev canopy-webpack-config
 ```
+
+Now create a webpack.config.js file. This file will use canopy-webpack-config and allow you to override anything you want to about the defaults. Inside of the
+file, call the `canopyWebpackConfig()` function to get some defaults and add/override to your webpack config. See example below:
 
 ```js
 // webpack.config.js
@@ -36,6 +36,21 @@ module.exports = canopyWebpackConfig('login-ui', {
 })
 ```
 
+Finally, create the following `scripts` in your package.json:
+
+```json
+{
+  "scripts": {
+    "build": "webpack --config webpack.config.js",
+    "analyze": "webpack --config webpack.config.js --env.analyze",
+    "start": "webpack-dev-server --config ./webpack.config.js --https --disable-host-check --cert ~/.canopy-ssl/public.pem --key ~/.canopy-ssl/key.pem --port"
+  }
+}
+```
+
+Now add `yarn build` to your `.gitlab-ci.yml` file for the build step. You can run `yarn analyze` at any time to see your bundle size and breakdown. And `yarn start`
+will start up a web server that is ready to go as a sofe override.
+
 ## API
 - `canopyWebpackConfig(name, config)`: The default export of the canopy-webpack-config npm library. This function requires both of its arguments.
   The first argument is a string name for the library you are exporting. The second is a webpack config that will be merged with the defaults that
@@ -44,6 +59,13 @@ module.exports = canopyWebpackConfig('login-ui', {
 
 ## Debugging
 To see your full webpack config, simply add a `console.log(module.exports)` inside of your webpack config.
+
+## Features and assumptions
+canopy-webpack-config assumes a few things about your project and provides defaults for those things:
+- It automatically uses babel-loader to compile js files.
+- It compiles your library to AMD format
+- It compiles your code into the `build` directory relative to where you started the webpack process.
+- It excludes all [common dependencies](git@code.canopy.ninja:front-end/gold/common-dependencies.git) from the build.
 
 ## Limitations
 the webpack config for canopy-webpack-config will always create the output bundle in the directory that the webpack process was started in. This
--- src/canopy-webpack-config.js
+++ src/canopy-webpack-config.js
@@ -1,65 +1,81 @@
-/* eslint-env node */
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const merge = require('webpack-merge')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = function(name, overridesConfig) {
   if (typeof name !== 'string') {
     throw new Error('canopy-webpack-config expects a string name as the first argument')
   }
 
-  if (typeof overridesConfig !== 'object') {
+  if (typeof overridesConfig !== 'object' && typeof overridesConfig !== 'function') {
     throw new Error('canopy-webpack-config expects an object as a second argument to override the canopy defaults. Received ' + typeof overridesConfig)
   }
 
-  const defaultCanopyConfig = {
-    entry: `./src/${name}.js`,
-    output: {
-      filename: `${name}.js`,
-      library: name,
-      libraryTarget: 'amd',
-      path: path.resolve(process.cwd(), 'build'),
-      chunkFilename: '[name].js',
-    },
-    mode: 'production',
-    module: {
-      rules: [
-        {
-          test: /\.js?$/,
-          exclude: [path.resolve(process.cwd(), 'node_modules')],
-          loader: 'babel-loader',
-        },
-      ],
-    },
-    resolve: {
-      modules: [
-        process.cwd(),
-        'node_modules',
-      ],
-    },
-    plugins: [
-      new CleanWebpackPlugin(['build']),
-    ],
-    devtool: 'source-map',
-    externals: [
-      /^.+!sofe$/,
-      /^lodash$/,
-      /^rxws$/,
-      /^moment$/,
-      /^react$/,
-      /^react\/lib.*/,
-      /^react-dom$/,
-      /.*react-dom.*/,
-      /^rx$/,
-      /^rxjs\/?.*$/,
-      /^rxws$/,
-      /^sofe$/,
-      /^single-spa$/,
-      /^single-spa-canopy$/,
-      /^prop-types$/,
-      /^canopy-sofe-extensions$/,
-    ],
-  };
+  return function(env) {
+    if (!env) {
+      env = {}
+    }
 
-  return merge.smart(defaultCanopyConfig, overridesConfig);
+    const defaultCanopyConfig = {
+      entry: `./src/${name}.js`,
+      output: {
+        filename: `${name}.js`,
+        library: name,
+        libraryTarget: 'amd',
+        path: path.resolve(process.cwd(), 'build'),
+        chunkFilename: '[name].js',
+      },
+      mode: env.dev ? 'development' : 'production',
+      module: {
+        rules: [
+          {
+            test: /\.js?$/,
+            exclude: [path.resolve(process.cwd(), 'node_modules')],
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      resolve: {
+        modules: [
+          process.cwd(),
+          'node_modules',
+        ],
+      },
+      plugins: [
+        new CleanWebpackPlugin(['build']),
+      ],
+      devtool: 'source-map',
+      externals: [
+        /^.+!sofe$/,
+        /^lodash$/,
+        /^rxws$/,
+        /^moment$/,
+        /^react$/,
+        /^react\/lib.*/,
+        /^react-dom$/,
+        /.*react-dom.*/,
+        /^rx$/,
+        /^rxjs\/?.*$/,
+        /^rxws$/,
+        /^sofe$/,
+        /^single-spa$/,
+        /^single-spa-canopy$/,
+        /^prop-types$/,
+        /^canopy-sofe-extensions$/,
+      ],
+    };
+
+    if (env.analyze) {
+      defaultCanopyConfig.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerPort: 1212
+        })
+      )
+    }
+
+    overridesConfig = typeof overridesConfig === 'function' ? overridesConfig(env) : overridesConfig
+
+    return merge.smart(defaultCanopyConfig, overridesConfig);
+  }
 }
